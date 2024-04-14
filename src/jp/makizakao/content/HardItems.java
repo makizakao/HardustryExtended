60cfcbaf69701268f05241094c7eeca42b583e92
makizakao <112055063+makizakao@users.noreply.github.com>
2024-04-15 00:02:10 +0900

    Added items.

--- src/jp/makizakao/content/HardItems.java
+++ src/jp/makizakao/content/HardItems.java
@@ -22,7 +22,7 @@ public class HardItems {
             ironBolt, ironRod, ironSawBlade, ironScrew, magneticIronRod,
             // metal
             aluminumIngot, brassIngot, bronzeIngot, copperIngot, goldIngot, IronIngot, leadIngot, silverIngot,
-            steelIngot, tinIngot, titaniumIngot, zincIngot,
+            steelIngot, tinIngot, zincIngot,
             // ore
             aurostibite, bauxite, clay, diamond, galena, hematite, magnetite, sphalerite, teallite, tetrahedrite,
             // other
@@ -65,17 +65,53 @@ public class HardItems {
         tinDust = createMaterialItem("tin-dust", 0.7f, DEFAULT_COLOR);
         woodPulp = new Item("wood-pulp", DEFAULT_COLOR);
         zincDust = createMaterialItem("zinc-dust", 0.7f, DEFAULT_COLOR);
+        // integrated logic circuit
+        coatedCircuitBoard = new Item("coated-circuit-board", DEFAULT_COLOR);
+        glassTube = new Item("glass-tube", DEFAULT_COLOR);
+        integratedLogicCircuit = createMaterialItem("integrated-logic-circuit", 5.0f, DEFAULT_COLOR);
+        resistor = new Item("resistor", DEFAULT_COLOR);
+        vacuumTube = new Item("vacuum-tube", DEFAULT_COLOR);
+        woodPlank = new Item("wood-plank", DEFAULT_COLOR);
+        // iron part
+        ironBolt = new Item("iron-bolt", DEFAULT_COLOR);
+        ironRod = new Item("iron-rod", DEFAULT_COLOR);
+        ironSawBlade = createMaterialItem("iron-saw-blade", 20f, DEFAULT_COLOR);
+        ironScrew = new Item("iron-screw", DEFAULT_COLOR);
+        magneticIronRod = new Item("magnetic-iron-rod", DEFAULT_COLOR);
         // metal
-        bronzeIngot = createMaterialItem("bronze-ingot", 1f, BRONZE_COLOR);
-        copperIngot = createMaterialItem("copper-ingot", 1f, DEFAULT_COLOR);
-        goldIngot = createMaterialItem("gold-ingot", 1f, DEFAULT_COLOR);
-        IronIngot = createMaterialItem("iron-ingot", 1f, DEFAULT_COLOR);
-        leadIngot = createMaterialItem("lead-ingot", 1f, DEFAULT_COLOR);
+        aluminumIngot = createMaterialItem("aluminum-ingot", 1f, DEFAULT_COLOR);
+        brassIngot = createMaterialItem("brass-ingot", 0.7f, DEFAULT_COLOR);
+        bronzeIngot = createMaterialItem("bronze-ingot", 0.7f, BRONZE_COLOR);
+        copperIngot = createMaterialItem("copper-ingot", 0.5f, DEFAULT_COLOR);
+        goldIngot = createMaterialItem("gold-ingot", 0.5f, DEFAULT_COLOR);
+        IronIngot = createMaterialItem("iron-ingot", 0.8f, DEFAULT_COLOR);
+        leadIngot = createMaterialItem("lead-ingot", 0.5f, DEFAULT_COLOR);
         silverIngot = createMaterialItem("silver-ingot", 1f, DEFAULT_COLOR);
+        steelIngot = createMaterialItem("steel-ingot", 1.2f, DEFAULT_COLOR);
+        tinIngot = createMaterialItem("tin-ingot", 0.6f, DEFAULT_COLOR);
+        zincIngot = createMaterialItem("zinc-ingot", 0.6f, DEFAULT_COLOR);
+        // ore
+        aurostibite = createOreItem("aurostibite", 2, DEFAULT_COLOR);
+        bauxite = createOreItem("bauxite", 4, DEFAULT_COLOR);
+        clay = createOreItem("clay", 0, DEFAULT_COLOR);
+        diamond = createOreItem("diamond", 4, DEFAULT_COLOR);
+        galena = createOreItem("galena", 4, DEFAULT_COLOR);
+        hematite = createOreItem("hematite", 3, DEFAULT_COLOR);
+        magnetite = createOreItem("magnetite", 3, DEFAULT_COLOR);
+        sphalerite = createOreItem("sphalerite", 3, DEFAULT_COLOR);
+        teallite = createOreItem("teallite", 2, DEFAULT_COLOR);
+        tetrahedrite = createOreItem("tetrahedrite", 2, DEFAULT_COLOR);
+        // other
+
     }
 
     // costのみ指定のItemを生成する
     private static Item createMaterialItem(String pName, float pCost, Color pColor) {
         return new Item(pName, pColor) {{ cost = pCost; }};
+    }
+
+    // hardnessのみ指定のItemを生成する
+    private static Item createOreItem(String pName, int pHardness, Color pColor) {
+        return new Item(pName, pColor) {{ hardness = pHardness; }};
     }
 }
